af84de998a36b7e7564f14365214b8c2d61674c8
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-02-27 05:37:11 +0900

    Develop dark theme

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="dark:bg-base transition duration-500">
+  <div class="_bg">
     <Header />
     <main class="w-full xs:w-11/12 mx-auto max-w-screen-xl mb-12">
       <Nuxt />
@@ -11,3 +11,17 @@
 <script>
 export default {}
 </script>
+
+<style lang="postcss">
+._text {
+  @apply dark:text-white transition duration-500;
+}
+
+._bg {
+  @apply bg-gray-200 dark:bg-gray-800 transition duration-500;
+}
+
+._bg-card {
+  @apply bg-gray-100 dark:bg-gray-700 transition duration-500;
+}
+</style>
