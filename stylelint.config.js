610e4406d2f67a80282f1d514d7c0aecca4e1696
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-02-27 01:31:29 +0900

    Fix stylelint setting for @apply

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,12 @@
 module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
-  // add your custom config here
-  // https://stylelint.io/user-guide/configuration
-  rules: {},
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-recess-order',
+    'stylelint-config-recommended-scss',
+    'stylelint-prettier/recommended',
+  ],
+  plugins: [],
+  rules: {
+    'scss/at-rule-no-unknown': [null],
+  },
 }
