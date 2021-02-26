01bd67e2b2ba57cdfac9b79c7d872217011af375
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-02-27 06:15:41 +0900

    Develop mode icon

--- components/DarkToggleButton.vue
+++ components/DarkToggleButton.vue
@@ -1,21 +1,23 @@
 <template>
-  <div>
-    <div
-      class="relative inline-block w-10 mr-2 align-middle select-none transition duration-200 ease-in"
-    >
+  <div class="flex">
+    <div class="relative inline-block w-10 mr-2 align-middle select-none">
       <input
         id="toggle"
         type="checkbox"
         name="toggle"
-        class="toggle-checkbox absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer outline-none transform transition duration-500"
+        class="toggle-checkbox absolute block w-6 h-6 rounded-full bg-gray-200 border-4 border-gray-700 appearance-none cursor-pointer outline-none transform transition duration-500"
         :checked="dark"
         @click="toggleDarkMode()"
       />
       <label
         for="toggle"
-        class="toggle-label block overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer"
+        class="toggle-label block overflow-hidden h-6 rounded-full bg-gray-200 border-2 border-gray-700 cursor-pointer transition duration-500"
       ></label>
     </div>
+    <transition name="fade" mode="out-in">
+      <Moon v-if="dark" class="mr-2" />
+      <Sun v-else class="mr-2" />
+    </transition>
   </div>
 </template>
 
@@ -47,9 +49,11 @@ export default {
       if (this.theme === 'dark') {
         document.documentElement.classList.remove('dark')
         this.theme = 'light'
+        this.dark = false
       } else {
         document.documentElement.classList.add('dark')
         this.theme = 'dark'
+        this.dark = true
       }
     },
   },
@@ -58,10 +62,20 @@ export default {
 
 <style lang="postcss" scoped>
 .toggle-checkbox:checked {
-  @apply translate-x-4 border-green-400;
+  @apply translate-x-4 bg-gray-800 border-gray-100;
 }
 
 .toggle-checkbox:checked + .toggle-label {
-  @apply bg-green-400;
+  @apply bg-gray-800 border-gray-100;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.25s;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
