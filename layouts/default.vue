2fd928994422deb6e73de158a945857245845aef
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-02-27 03:45:36 +0900

    Add DarkToggleButton

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,36 @@
 <template>
-  <div class="bg-base">
+  <div class="dark:bg-base transition duration-500">
     <Header />
+    <DarkToggleButton :is-dark="dark" :is-theme="theme" />
     <main class="w-full xs:w-11/12 mx-auto max-w-screen-xl mb-12">
       <Nuxt />
     </main>
     <Footer />
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      theme: 'light',
+      dark: true,
+    }
+  },
+  mounted() {
+    if (
+      localStorage.theme === 'dark' ||
+      (!('theme' in localStorage) &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches)
+    ) {
+      document.documentElement.classList.add('dark')
+      this.theme = 'dark'
+      this.dark = true
+    } else {
+      document.documentElement.classList.remove('dark')
+      this.theme = 'right'
+      this.dark = false
+    }
+  },
+}
+</script>
