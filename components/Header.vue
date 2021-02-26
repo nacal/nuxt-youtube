3571d70816bbc1347ec4686ab12c21f22cbfbace
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-02-27 06:15:51 +0900

    Fix layout

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,7 @@
 <template>
-  <header class="w-full bg-primary py-4 flex justify-center relative">
+  <header
+    class="w-full bg-primary py-4 flex justify-center align-center relative"
+  >
     <h1 class="text-xl font-bold text-center">NewRadio</h1>
     <DarkToggleButton class="absolute right-0" />
   </header>
