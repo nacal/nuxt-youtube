55e4cf48a499ed6b27d851d4d648c46b84e0f4e5
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-03-12 12:41:15 +0900

    Change title style

--- components/Header.vue
+++ components/Header.vue
@@ -3,7 +3,11 @@
     class="w-full _bg-banner py-4 flex justify-center items-center relative"
   >
     <Logo />
-    <h1 class="text-xl font-bold text-center _text">NewRadio</h1>
+    <h1
+      class="ml-1 text-xl font-medium text-center _text transform scale-y-105 scale-x-90"
+    >
+      NewRadio
+    </h1>
     <DarkToggleButton class="absolute right-0" />
   </header>
 </template>
