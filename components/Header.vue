f88deb9f8e8b35acacca7a0727f65bcec22d59a4
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-03-12 12:31:02 +0900

    Fix typo class name

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header
-    class="w-full _bg-banner py-4 flex justify-center align-center relative"
+    class="w-full _bg-banner py-4 flex justify-center items-center relative"
   >
     <Logo />
     <h1 class="text-xl font-bold text-center _text">NewRadio</h1>
