9663c019200b0c6cb87dbd8650452df72d232cfd
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-03-12 12:28:39 +0900

    Add Logo SVG component

--- components/Header.vue
+++ components/Header.vue
@@ -2,6 +2,7 @@
   <header
     class="w-full _bg-banner py-4 flex justify-center align-center relative"
   >
+    <Logo />
     <h1 class="text-xl font-bold text-center _text">NewRadio</h1>
     <DarkToggleButton class="absolute right-0" />
   </header>
